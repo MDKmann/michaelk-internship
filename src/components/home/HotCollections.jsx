c9af1e9fcb2f189e4c77853a4e6f5cf10a674c81
Michael <MDKuhlmann@zoho.com>
2025-01-20 22:32:43 -0600

    added responsive breakpoints to react-slick carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -13,6 +13,32 @@ const HotCollections = () => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
   };
 
   const fetchData = useCallback(async () => {
