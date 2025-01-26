a0f4984ee02e206cfb0f95ba08d583062e84a3f3
Michael <MDKuhlmann@zoho.com>
2025-01-25 19:57:53 -0600

    added dynamic data via fetching data from api and incorporated skeleton loading state using Skeleton component

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellersData, setTopSellersData] = useState([]);
+
+  const fetchTopSellers = useCallback(async () => {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setTopSellersData(data);
+  }, []);
+
+  useEffect(() => {
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +29,53 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {topSellersData.length ? (
+                <>
+                  {topSellersData.map((seller, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId}`}>{seller.authorName}</Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to="/author">
+                          <Skeleton
+                            width="50px"
+                            height="50px"
+                            borderRadius="50%"
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to="/author">
+                          <Skeleton width="120px" height="25px" />
+                        </Link>
+                        <span>
+                          <Skeleton width="70px" height="20px" />
+                        </span>
+                      </div>
+                    </li>
+                  ))}
+                </>
+              )}
             </ol>
           </div>
         </div>
