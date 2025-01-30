b366c3105900272db8d330169c9e71a0db9a5f9b
Michael <MDKuhlmann@zoho.com>
2025-01-30 10:20:06 -0600

    removed accidental backticks

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link, useParams } from "react-router-dom";``
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Skeleton from "../components/UI/Skeleton";
 
